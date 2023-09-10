99aeb1f3e628550a80b20cd38572eb7f47e88e8e
Bruno Magagnin Pereira <bruno_magagnin@yahoo.com.br>
2023-09-10 15:56:43 -0300

    Revert "Adicionando modificação projeto person: telas, alertas e outros"
    This reverts commit e87667244cafceae05385b4a49aad5d6db2bd5e1.

--- Detection_Proximity_person/src/Control/control.cpp
+++ Detection_Proximity_person/src/Control/control.cpp
@@ -1,12 +1,27 @@
 #include "control.h"
 
+/*++++++++++++++++++++++++  ALERT  +++++++++++++++++++++++
+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
 bool controlAlert = false;
 uint32_t lastAlert = 0;
+bool acceptedApproximation = false;
+
+/*++++++++++++++++++++++++  ALERT  +++++++++++++++++++++++
+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
+bool iconCreate[NUMBER_OF_PEERS] = {false, false};
+
+/*++++++++++++++++++++++++  TIMER  +++++++++++++++++++++++
+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
+bool initTimer = false;
+uint32_t setTime = 0;
+const uint32_t setPoint = 20000;
 
 namespace controller
 {
-    void alert(ESP_Peer *peers) //
+    void alert(ESP_Peer *peers, bool accepted) //
     {
+        acceptedApproximation = accepted;
+
         // Tempo mínimo para novo alerta
         if (millis() - lastAlert > REFRESH)
         {
@@ -22,7 +37,7 @@ namespace controller
                 }
             }
             // ALERTA
-            if (controlAlert)
+            if (!acceptedApproximation && controlAlert)
             {
                 lastAlert = millis();
                 M5.Axp.SetLDOEnable(3, true);
@@ -39,10 +54,43 @@ namespace controller
             controlAlert = false;
             for (int i = 0; i < NUMBER_OF_PEERS; i++)
             {
-               peers[i].setIsAlert(false);
+                peers[i].setIsAlert(false);
             }
         }
-         
     }
-   
+/*
+    bool iconAlert(ESP_Peer *peers, bool accepted)
+    {
+        if (!iconCreate)
+        {
+            for (int index = 0; index < NUMBER_OF_PEERS; index++)
+            {
+                if (peers->getAcceptClose() && !iconCreate[index])
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+*/
+    bool counterTime(ESP_Peer *peers)
+    {
+        if (!initTimer)
+        {
+            initTimer = true;
+            setTime = millis();
+        }
+
+        if (millis() - setTime > setPoint)
+        {
+            setTime = millis();
+            return false;
+        }
+        for (int i = 0; i < 2; i++)
+        {
+            peers->setAcceptClose(false);
+        }
+        return true;
+    }
 }
